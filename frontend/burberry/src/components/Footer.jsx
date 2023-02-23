7fbcae7d43e3ecece882a498a8aceecbdc76d44e
piyushkhurana8744 <piyushkhurana8744@gmail.com>
2023-02-23 10:31:31 +0530

    homepage

---
+++ frontend/burberry/src/components/Footer.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import {Box,Image,Flex,Text,Icon,Grid,Input} from "@chakra-ui/react"
+const Footer = () => {
+  return (
+    <div>
+      <Box id="footer" paddingBottom={"40px"}>
+               <Flex justifyContent={"space-around"}>
+                <Box width="25%">
+                    <Text>
+                        SIGN UP
+                    </Text>
+                    <Text fontSize="16px" textAlign={"start"}>
+                    Sign up below for updates about the world of Burberry, including collection launches and early access to limited-edition products and collaborations.
+                    </Text>
+                    <Input placeholder="Email" border="none"></Input>
+                </Box>
+                <Box>
+                    <Text>STORE LOCATOR</Text>
+                </Box>
+                <Box>
+                    <Text>
+                    CUSTOMER SERVICE
+                    </Text>
+                    <Text>
+                    LEGAL & COOKIES
+                    </Text>
+                    <Text>
+                    OUR COMPANY
+                    </Text>
+                </Box>
+                <Box>
+                    <Text>
+                    LANGUAGE
+                    </Text>
+                    <Text color="grey">
+                    ENGLISH
+                    </Text>
+                    <Text>SHIPPING TO</Text>
+                    <Text color="grey">INDIA</Text>
+                </Box>
+               </Flex>
+            </Box>
+            <hr marginTop="20px"/>
+            <Box textAlign={"start"} width="90%" margin={"auto"} lineHeight="10px" paddingBottom={"20px"}>
+                <Text fontSize={"14px"} color="grey">If you are using a screen-reader and are having problems using this website, please call variable_country_customer_service_phone or contact us for assistance.</Text>
+                <Text fontSize={"14px"} color="grey">Burberry Limited, Horseferry House, Horseferry Road, London, SW1P 2AW</Text>
+                <Text fontSize={"14px"} color="grey">Registered in England & Wales</Text>
+                <Text fontSize={"14px"} color="grey">Registered Company Number: 00162636</Text>
+            </Box>
+    </div>
+  )
+}
+
+export default Footer
---
+++ frontend/burberry/src/pages/homepage.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import {Box,Image,Flex,Text,Icon,Grid,Input} from "@chakra-ui/react"
+import Footer from '../components/Footer'
+const Homepage = () => {
+  return (
+    <div>
+        <Box>
+            <Grid templateColumns={"repeat(2,1fr)"} gap="5px" >
+                <Box >
+                <Image src="https://assets.burberry.com/is/image/Burberryltd/7D3E2F24-04F9-4837-8CF8-2DDE40B0BA52?$BBY_V2_ML_1x1$&wid=1251&hei=1251" width="100%"  ></Image>
+                <Text fontSize="30px" fontWeight={"300"} marginTop="-10px" backgroundColor={"#e9e9e9"} color="#4a4a4a" paddingBottom={"10px"}>
+                    Women's New Arrival
+                </Text>
+                </Box>
+                <Box  paddingBottom={"10px"}>
+                <Image src="https://assets.burberry.com/is/image/Burberryltd/DCD5E15F-93F0-4C2E-9A38-B381B4AB7326?$BBY_V2_ML_1x1$&wid=1251&hei=1251" width="100%"  ></Image>
+                <Text fontSize="30px" fontWeight={"300"} marginTop="-10px" backgroundColor={"#e9e9e9"} color="#4a4a4a" paddingBottom={"10px"}>
+                    Men's New Arrival
+                </Text>
+                </Box>
+            </Grid>
+            <Grid templateColumns={"repeat(2,1fr)"} >
+                <Box >
+                <Image src="https://www.fashiongonerogue.com/wp-content/uploads/2023/02/Burberry-Daniel-Lee-2023-Campaign01.jpg" width="100%" height="600px"></Image>
+                <Text fontSize="30px" fontWeight={"300"} marginTop="-10px" backgroundColor={"#030406"} color="white" paddingBottom={"10px"} textAlign="right">
+                        The Herita
+                </Text>
+                </Box>
+                <Box  paddingBottom={"10px"}>
+                <Image src="https://mediaslide-europe.storage.googleapis.com/success/news_pictures/2023/02/large-1675680362-7d170db1479af6fa18591034f2c6b110.jpg" width="100%" height="600px"></Image>
+                <Text fontSize="30px" fontWeight={"300"} marginTop="-10px" backgroundColor={"#d9d8db"} color="white" paddingBottom={"10px"} textAlign="left">
+                    geCllection
+                </Text>
+                </Box>
+                
+            </Grid>
+            <Box
+              w={"99%"}
+              p={2}
+              height={"800px"}
+              margin="auto"
+              backgroundImage="url('https://assets.burberry.com/is/image/Burberryltd/513CC023-678E-4793-925E-CEE0067DF98E?wid=1366')"
+            >
+             <Text fontSize={"40px"} color="white" marginTop="670px">Bags</Text>
+            </Box>
+            <Box>
+               <Text fontSize={"38px"} fontWeight="500">Children</Text>
+               <Text fontSize={"38px"} fontWeight="500" marginTop='-20px'>The Trench Coat</Text>
+               <Text fontSize={"38px"} fontWeight="500" marginTop='-20px'>SCARVES</Text>
+            </Box>
+            <hr />
+        </Box>
+        <Footer />
+    </div>
+  )
+}
+
+export default Homepage

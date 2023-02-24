08d94e0f94ff1c4d6d46539614d7613678affe93
ayushgupta1324 <ayushgupta1324@gmail.com>
2023-02-24 15:52:28 +0530

    routing

---
+++ backend/model/Women.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const womenSchema = mongoose.Schema({
+    product_img:{type:String,required: true},
+    product_title:{type:String,required: true},
+    product_price:{type:String,required: true},
+    product_category:{type:String,required: true},
+    quantity:{type:String,required: true}
+})
+
+const WomenModel = mongoose.model("women",productSchema)
+
+module.exports = {
+    WomenModel
+}
